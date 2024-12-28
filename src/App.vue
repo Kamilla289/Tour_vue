8ea9de21a5d65633a9306f2db2e5980fced18ed8
Kamilla289 <kami.rivay56@gmail.com>
2024-12-28 17:00:17 +0500

    add: files

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import HeroView from "@/views/HeroView.vue";
+import FooterComponent from "@/components/FooterComponent.vue";
+
+export default {
+  components: {
+    HeroView,
+    FooterComponent, // Футер остаётся общим для всех страниц
+  },
+};
+</script>
+
+<template>
+  <div id="wrapper">
+    <router-view />
+    <!-- Динамическое отображение компонентов -->
+    <footer-component />
+    <!-- Футер всегда виден -->
+  </div>
+</template>
